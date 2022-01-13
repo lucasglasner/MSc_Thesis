94e6f7002b26eef10ba1c8ad8149d8f9e234de90
LucasG <lgvivanco96@gmail.com>
2022-01-13 17:59:33 -0300

    unos cuantos mapas con estadisticas ROS

--- scripts/ROS_CORTESCR2ERA5_analysis.py
+++ scripts/ROS_CORTESCR2ERA5_analysis.py
@@ -10,6 +10,7 @@ Created on Tue Jan  4 10:12:44 2022
 # =============================================================================
 """
 
+from scipy.stats import linregress
 import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
@@ -65,7 +66,28 @@ for yr in range(1984, 2016, 1):
     maxSWE.append(SWE)
 mask = xr.concat(maxSWE, 'max_years').mean(dim='max_years') > 20
 del maxSWE
+# %%
+dSWE = xr.open_mfdataset(
+    glob('datos/ANDES_SWE_Cortes/regrid_cr2met/ANDES_dSWE_*')).SWE
+dSWE = dSWE.where(ROS.reindex({'time': dSWE.time}) == True)
+dSWE = dSWE.resample({'time': 'y'}).min().mean(dim='time')
 
+# %%
+
+
+trend = ROS.resample({'time': 'y'}).sum().load()
+trend_new = np.empty((trend.shape[1], trend.shape[2]))
+pvalues = np.empty((trend.shape[1], trend.shape[2]))
+
+for i in range(trend.shape[1]):
+    for j in range(trend.shape[2]):
+        t = linregress(np.linspace(0, 1, trend.shape[0]), trend[:, i, j])
+        trend_new[i, j] = t.slope
+        pvalues[i, j] = t.pvalue
+trend = xr.DataArray(trend_new, coords=[trend.lat, trend.lon],
+                     dims=['lat', 'lon'])
+pvalues = xr.DataArray(pvalues, coords=[trend.lat, trend.lon],
+                       dims=['lat', 'lon'])
 # %%
 # =============================================================================
 # Create Data for maps
@@ -78,6 +100,71 @@ mean_rainydays = mean_rainydays.where(mask).reindex({'lat': ROS_days.lat,
 timing = timing.where(mask.values)
 
 
+# %%
+fig, ax = plt.subplots(1, 4, sharex=True, sharey=True,
+                       subplot_kw={"projection": ccrs.PlateCarree()},
+                       figsize=(18, 6))
+
+
+plt.rcParams.update({'font.size': 18})
+ax = ax.ravel()
+for axis in ax:
+    axis.set_extent([-74, -68, -26, -38])
+    # axis.set_extent([-74, -69.7, -33, -34.32], crs=ccrs.PlateCarree())
+    axis.coastlines(rasterized=True)
+    axis.add_feature(cf.BORDERS)
+    gl = axis.gridlines(linestyle=":")
+    gl.xlocator = mticker.FixedLocator([])
+    gl.ylocator = mticker.FixedLocator([-37, -35, -33, -31, -29, -27])
+    gl.top_labels = False
+    gl.right_labels = False
+    gl.xlines = False
+    cuencas.boundary.plot(ax=axis, color="k", lw=0.5)
+    axis.add_feature(cf.LAND, color='k', alpha=0.2, rasterized=True)
+    axis.add_feature(cf.OCEAN, rasterized=True)
+
+
+mapa0 = ax[0].pcolormesh(LON, LAT, dSWE.where(mask),
+                         cmap="Greens_r",
+                         transform=ccrs.PlateCarree(),
+                         rasterized=True,
+                         norm=mpl.colors.Normalize(-25, 0))
+mapa1 = ax[1].pcolormesh(LON, LAT,
+                         trend.where(mask).where(trend != 0),
+                         cmap="RdBu_r",
+                         transform=ccrs.PlateCarree(),
+                         rasterized=True,
+                         norm=mpl.colors.Normalize(-12, 12))
+mapa2 = ax[2].pcolormesh(LON, LAT,
+                         pvalues.where(mask).where(trend != 0),
+                         cmap='Purples',
+                         transform=ccrs.PlateCarree(),
+                         rasterized=True,
+                         norm=mpl.colors.Normalize(0, 1))
+
+# ax[1].pcolormesh(LON, LAT,
+#                  pvalues.where(mask).where(pvalues > 0.05).where(trend != 0),
+#                  cmap="bone", alpha=0.1, vmin=1, vmax=1,
+#                  rasterized=True, linewidth=0)
+
+
+ax[0].set_title("Maximum SWE loss\non ROS days\n"+r"($mm$)")
+ax[1].set_title("Yearly ROS\ndays Trend\n"+r"($\frac{N°ROS Days}{year}$)")
+ax[2].set_title("Yearly ROS\ndays Trend\npvalue"+"\n(-)")
+
+cb0 = fig.colorbar(mapa0, ax=ax[0], aspect=40)
+cb1 = fig.colorbar(mapa1, ax=ax[1], aspect=40)
+cb2 = fig.colorbar(mapa2, ax=ax[2], aspect=40)
+
+
+gl = ax[0].gridlines(draw_labels=True, linestyle=":")
+gl.xlocator = mticker.FixedLocator([])
+gl.ylocator = mticker.FixedLocator([-37, -35, -33, -31, -29, -27])
+gl.top_labels = False
+gl.right_labels = False
+gl.xlines = False
+gl.ylines = False
+plt.savefig('plots/ROS_STATS.pdf', dpi=150, bbox_inches='tight')
 # %%
 fig, ax = plt.subplots(1, 4, sharex=True, sharey=True,
                        subplot_kw={"projection": ccrs.PlateCarree()},
@@ -128,8 +215,10 @@ cb0 = fig.colorbar(mapa0, ax=ax[0], aspect=40)
 cb1 = fig.colorbar(mapa1, ax=ax[1], aspect=40)
 cb2 = fig.colorbar(mapa2, ax=ax[2], aspect=40)
 cb2 = fig.colorbar(mapa3, ax=ax[3], aspect=40, ticks=np.arange(1, 13, 1))
+# cb2 = fig.colorbar(mapa3, ax=ax[3], aspect=40, ticks=np.linspace(1, 365, 12))
 cb2.set_ticklabels(["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
                     "JUL", "AUG", "SEP", "OCT", "NOV", "DIC"])
+
 
 gl = ax[0].gridlines(draw_labels=True, linestyle=":")
 gl.xlocator = mticker.FixedLocator([])
